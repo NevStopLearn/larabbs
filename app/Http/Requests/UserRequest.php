300e672b3d51acc8d6cf05e92cb39ddd9d8c7f0d
BuLing <BuLing@qq.com>
2019-01-30 16:59:19 +0800

    限制头像分辨率

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,6 +28,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar' => 'mimes:jpeg,jpg,bmp,png,gif|dimensions:min_width=208,min_height=208',
         ];
     }
 
@@ -40,6 +41,8 @@ class UserRequest extends FormRequest
             'name.required'     => '用户名不能为空。',
             'email.required'    => '邮箱不能为空。',
             'introduction.max'  => '个人简介最多80个字符。',
+            'avatar.mimes'      => '头像必须是 jpeg, jpg, png, gif 格式的图片',
+            'avatar.dimensions' => '图片的清晰度不够，宽和高需要 208px 以上',
         ];
     }
 }
